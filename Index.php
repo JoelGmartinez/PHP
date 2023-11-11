12d01c21727426ec88ed84a93461c9ceb4b6ed19
Joel <joelstiven195@gmail.com>
2023-11-11 18:02:09 -0500

    3er Commit

--- Index.php
+++
@@ -1,9 +0,0 @@
-<?php include("header.php")?>
-<section>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto explicabo porro quam minima tenetur soluta aperiam magni, velit consectetur ipsam provident veniam fuga esse, magnam enim itaque distinctio delectus? Recusandae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, consequuntur. Cupiditate eos corporis quo ea deserunt aut dolorem labore, enim nemo, iure voluptatum debitis consequatur iusto dolorum odio qui incidunt?
-        </p>
-    </section>
-    
-</body>
-</html>
--- header.php
+++ header.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <nav>
-            <div>
-                <ul>
-                    Logo
-                </ul>
-            </div>
 
-            <ul>
-                <li>Inicio</li>
-                <li>Nosotros</li>
-                <li>Servicios</li>
-                <li>Contacto</li> 
-            </ul>
-            
-        </nav>
-
-    </header>
-
-    <section>
-        <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" alt="">
-    </section>
 
     <section>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto explicabo porro quam minima tenetur soluta aperiam magni, velit consectetur ipsam provident veniam fuga esse, magnam enim itaque distinctio delectus? Recusandae!
@@ -38,3 +8,4 @@
     
 </body>
 </html>
+
